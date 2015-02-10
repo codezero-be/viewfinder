7926b9bf07e433ff8037b6590fa1dcf518eef0dc
Ivan Vermeyen <ivan@vermeyen.org>
2015-02-10 02:29:07 +0100

    Optimize config file

--- src/config/config.php
+++ src/config/config.php
@@ -2,56 +2,37 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Primary Prefix/Locale
-    |--------------------------------------------------------------------------
-    |
-    | This prefix will be prepended to the view and will be searched for first.
-    | In most cases this will be the current locale.
-    |
-    */
-    'primary' => Config::get('app.locale'),
+    /**
+     * Primary Prefix/Locale
+     *
+     * This prefix will be prepended to the view name
+     * and will be searched for first.
+     */
+    'primaryPrefix' => Config::get('app.locale'),
 
-    /*
-    |--------------------------------------------------------------------------
-    | Fallback Prefix/Locale
-    |--------------------------------------------------------------------------
-    |
-    | This prefix will be prepended to the view and will be searched for
-    | when the view is not found in the primary location.
-    | In most cases this will be the fallback locale.
-    |
-    */
-    'fallback' => Config::get('app.fallback_locale'),
+    /**
+     * Fallback Prefix/Locale
+     *
+     * This prefix will be prepended to the view name
+     * and will be searched for when the view is not
+     * found in the primary location.
+     */
+    'fallbackPrefix' => Config::get('app.fallback_locale'),
 
-    /*
-    |--------------------------------------------------------------------------
-    | List of additional Prefixes/Locales
-    |--------------------------------------------------------------------------
-    |
-    | If the primary and fallback locations are invalid
-    | these additional prefixes will be used to find the view.
-    |
-    | If you want, you can add an array to the main app/config/app.php file:
-    | 'locales' => ['nl' => 'Dutch', 'en' => 'English', 'fr' => 'French']
-    | and swap the following 'prefixes' setting.
-    |
-    | This way, you can also use they locale values for PHP's
-    | setlocale(LC_ALL, $locales[$locale]);
-    |
-    */
-    //'prefixes' => array_keys(Config::get('app.locales')),
-    'prefixes' => [],
-
-    /*
-    |--------------------------------------------------------------------------
-    | View Directory Separator
-    |--------------------------------------------------------------------------
-    |
-    | Do you prefer 'en.folder.view' or 'en/folder/view'?
-    |
-    */
-    'divider' => '.',
+    /**
+     * List of Prefixes/Locales
+     *
+     * If a view isn't found in the primary or fallback location,
+     * these prefixes will be used to continue the search.
+     *
+     * If you want, you can add an array to the main config/app.php file:
+     * 'locales' => ['nl' => 'Dutch', 'en' => 'English', 'fr' => 'French'],
+     *
+     * This way, you can also use these locale values for PHP's localization:
+     * setlocale(LC_ALL, $locales[$locale]);
+     *
+     * You may include the primary and fallback locale in this array.
+     */
+    'prefixes' => Config::has('app.locales') ? array_keys(Config::get('app.locales')) : [],
 
 ];
